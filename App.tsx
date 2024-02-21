573d98ed66b3f46bb3ff422ba72d994b1130082d
Ahmad Zayed <ahmadzayed.2211@gmial.com>
2024-02-21 17:29:15 +0200

    Project setup

--- App.tsx
+++ App.tsx
@@ -76,11 +76,11 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Stepa One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
+          <Section title="See Your Changes!!">
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
